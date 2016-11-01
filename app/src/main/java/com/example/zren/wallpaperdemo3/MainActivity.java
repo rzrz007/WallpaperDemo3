4a2d05fd38ef326e6a3502414fb506f40e956c0a
84168 <841682148@qq.com>
2016-11-01 09:24:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zren/wallpaperdemo3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.zren.wallpaperdemo3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("这是没有忽略任何文件的");
+    }
+}
