1db92808a76870c18954b605b5aab0cce14f0262
GRH <icetea92930>
2016-11-01 10:04:27 +0800

    three test

--- app/src/main/java/com/example/zren/wallpaperdemo3/MainActivity.java
+++ app/src/main/java/com/example/zren/wallpaperdemo3/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("这是没有忽略任何文件的");
 
+        System.out.println("three test");
         System.out.println("第二次测试");
     }
 }
