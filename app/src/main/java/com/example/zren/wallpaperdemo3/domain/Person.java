9e2114e6b7981ec25c4026eae7c5a8e0978d2585
ysy <291321903@qq.com>
2016-11-01 10:50:21 +0800

    domain

---
+++ app/src/main/java/com/example/zren/wallpaperdemo3/domain/Person.java
@@ -0,0 +1,16 @@
+package com.example.zren.wallpaperdemo3.domain;
+
+/**
+ * Created by ysy on 2016/11/1.
+ */
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
