48584838bbbce7da36c43f94a1f017ca1ab312a8
landsky626 <1607360319@qq.com>
2016-11-01 09:30:21 +0800

    第二次上传

--- app/src/main/java/com/example/zren/wallpaperdemo3/MainActivity.java
+++ app/src/main/java/com/example/zren/wallpaperdemo3/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("这是没有忽略任何文件的");
+
+        System.out.println("第二次测试");
     }
 }
