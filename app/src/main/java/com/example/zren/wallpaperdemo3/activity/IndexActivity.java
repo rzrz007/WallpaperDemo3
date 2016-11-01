437270b0c8f0f31ddd4b710e90e7b3aa7bf106c4
landsky626 <1607360319@qq.com>
2016-11-01 22:03:47 +0800

    优化了IndexActivity的视觉效果

--- app/src/main/java/com/example/zren/wallpaperdemo3/activity/IndexActivity.java
+++ app/src/main/java/com/example/zren/wallpaperdemo3/activity/IndexActivity.java
@@ -1,25 +1,35 @@
 package com.example.zren.wallpaperdemo3.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.zren.wallpaperdemo3.R;
-import com.example.zren.wallpaperdemo3.fragment.Kind_Fragment;
+import com.example.zren.wallpaperdemo3.fragment.Category_Fragment;
 import com.example.zren.wallpaperdemo3.fragment.Recommend_Fragment;
 import com.example.zren.wallpaperdemo3.fragment.Search_Fragment;
 
-public class IndexActivity extends AppCompatActivity {
+public class IndexActivity extends AppCompatActivity implements View.OnClickListener {
     private long preTime;
-    private RadioGroup radioGroup;
-    private RadioButton radioButton01,radioButton02,radioButton03;
-    private Fragment fragment;
+
+    private LinearLayout layout_recommand, layout_category, layout_search;
+
+    private ImageView imageView_recommand,imageView_category,imageView_search;
+
+    private TextView textView_recommand,textView_category,textView_search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,61 +38,84 @@ public class IndexActivity extends AppCompatActivity {
 
         initView();
 
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId){
-                    case R.id.rb_01:
-                        showFirstFragment();
-                        break;
-                    case R.id.rb_02:
-                        showSecondFragment();
-                        break;
-                    case R.id.rb_03:
-                        showThirdFragment();;
-                        break;
-                }
-
-            }
-        });
 
     }
 
     public void initView() {
-        radioGroup = (RadioGroup) this.findViewById(R.id.radioGroup_IndexActivity);
-        radioButton01 = (RadioButton) this.findViewById(R.id.rb_01);
-        radioButton02 = (RadioButton) this.findViewById(R.id.rb_02);
-        radioButton03 = (RadioButton) this.findViewById(R.id.rb_03);
+        layout_recommand = (LinearLayout) this.findViewById(R.id.layout_recommand);
+        layout_category = (LinearLayout) this.findViewById(R.id.layout_category);
+        layout_search = (LinearLayout) this.findViewById(R.id.layout_search);
 
-        //默认显示第一个碎片：“精选”部分
+        layout_recommand.setOnClickListener(this);
+        layout_category.setOnClickListener(this);
+        layout_search.setOnClickListener(this);
+
+        imageView_recommand = (ImageView) this.findViewById(R.id.imageView_recommand);
+        imageView_category = (ImageView) this.findViewById(R.id.imageView_category);
+        imageView_search = (ImageView) this.findViewById(R.id.imageView_search);
+
+        textView_recommand = (TextView) this.findViewById(R.id.textView_recommand);
+        textView_category = (TextView) this.findViewById(R.id.textView_category);
+        textView_search = (TextView) this.findViewById(R.id.textView_search);
+
+
+        //默认显示第一个碎片：“推荐”部分
         showFirstFragment();
     }
 
-    //显示 “精选” 碎片
-    private void showFirstFragment() {
-        if(!(fragment instanceof Recommend_Fragment)){
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.linearLayout_for_fragment,Recommend_Fragment.getInstance());
-            ft.commit();
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.layout_recommand:
+                imageView_recommand.setImageResource(R.mipmap.bottom_recommand_selected);
+                textView_recommand.setTextColor(getResources().getColor(R.color.textColor));
+                imageView_category.setImageResource(R.mipmap.bottom_category);
+                textView_category.setTextColor(Color.GRAY);
+                imageView_search.setImageResource(R.mipmap.bottom_search);
+                textView_search.setTextColor(Color.GRAY);
+                showFirstFragment();
+                break;
+            case R.id.layout_category:
+                imageView_recommand.setImageResource(R.mipmap.bottom_recommand);
+                textView_recommand.setTextColor(Color.GRAY);
+                imageView_category.setImageResource(R.mipmap.bottom_category_selected);
+                textView_category.setTextColor(getResources().getColor(R.color.textColor));
+                imageView_search.setImageResource(R.mipmap.bottom_search);
+                textView_search.setTextColor(Color.GRAY);
+                showSecondFragment();
+                break;
+            case R.id.layout_search:
+                imageView_recommand.setImageResource(R.mipmap.bottom_recommand);
+                textView_recommand.setTextColor(Color.GRAY);
+                imageView_category.setImageResource(R.mipmap.bottom_category);
+                textView_category.setTextColor(Color.GRAY);
+                imageView_search.setImageResource(R.mipmap.bottom_search_selected);
+                textView_search.setTextColor(getResources().getColor(R.color.textColor));
+                showThirdFragment();
+                break;
         }
+    }
+
+    //显示 “推荐” 碎片
+    private void showFirstFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.linearLayout_for_fragment, Recommend_Fragment.getInstance());
+        fragmentTransaction.commit();
+
     }
 
     //显示 “分类” 碎片
-    private void showSecondFragment(){
-        if(!(fragment instanceof Kind_Fragment)){
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.linearLayout_for_fragment,Kind_Fragment.getInstance());
-            ft.commit();
-        }
+    private void showSecondFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.linearLayout_for_fragment, Category_Fragment.getInstance());
+        fragmentTransaction.commit();
     }
 
     //显示 “搜索” 碎片
-    private void showThirdFragment(){
-        if(!(fragment instanceof Search_Fragment)){
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.linearLayout_for_fragment,Search_Fragment.getInstance());
-            ft.commit();
-        }
+    private void showThirdFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.linearLayout_for_fragment, Search_Fragment.getInstance());
+        fragmentTransaction.commit();
     }
 
 
@@ -110,4 +143,6 @@ public class IndexActivity extends AppCompatActivity {
         }
         return super.onKeyUp(keyCode, event);
     }
+
+
 }
