0cb031ea347e792cc6174db837c749f4cd145f9f
GRH <icetea92930>
2016-11-07 09:58:22 +0800

    grh

---
+++ app/src/main/java/com/example/zren/wallpaperdemo3/activity/Activity_Serach_10sort.java
@@ -0,0 +1,17 @@
+package com.example.zren.wallpaperdemo3.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.example.zren.wallpaperdemo3.R;
+
+public class Activity_Serach_10sort extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_activity__serach_10sort);
+    }
+}
